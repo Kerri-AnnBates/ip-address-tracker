737729186ae5e1810ac5413fcc933b15240f0602
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-10-24 23:25:15 -0400

    styled data block

--- src/components/IPDataBlock.js
+++ src/components/IPDataBlock.js
@@ -3,7 +3,24 @@ import React from "react";
 const IPDataBlock = () => {
     return (
         <div className="ip-data">
-            IP data goes here
+            <div className="flex-parent data-container">
+                <div className="flex-item data-block">
+                    <h2 className="sub-title">Ip Address</h2>
+                    <p className="ip-data-data ip-address">192.212.174.101</p>
+                </div>
+                <div className="flex-item data-block">
+                    <h2 className="sub-title">Location</h2>
+                    <p className="ip-data-data ip-location">Brooklyn, NY 10001</p>
+                </div>
+                <div className="flex-item data-block">
+                    <h2 className="sub-title">Timezone</h2>
+                    <p className="ip-data-data ip-timezone">UTC -05:00</p>
+                </div>
+                <div className="flex-item data-block">
+                    <h2 className="sub-title">ISP</h2>
+                    <p className="ip-data-data">SpaceX Starlink</p>
+                </div>
+            </div>
         </div>
     )
 }
