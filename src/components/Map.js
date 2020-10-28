c92dad21fedf4f021caace78363193204ab96270
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-10-27 21:21:56 -0400

    install leaflet

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,9 +2,7 @@ import React from "react";
 
 const Map = () => {
     return (
-        <div className="map">
-            Map goes here
-        </div>
+        <div className="map" id="ip-map"></div>
     )
 }
 
