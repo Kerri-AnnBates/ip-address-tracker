3ffb6119922ba259862aa704480ec5c14b8f5b02
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-10-29 09:54:59 -0400

    added map

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import mapMarker from "../images/icon-location.svg";
 
 const MapDisplay = () => {
     const [viewport, setViewport] = useState({
@@ -7,15 +8,28 @@ const MapDisplay = () => {
         width: "100vw",
         latitude: 28.800791,
         longitude: -81.273109,
-        zoom: 10
+        zoom: 12
     });
 
     return (
-        <ReactMapGL
-            className="map"
-            {...viewport}
-            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API}
-        ></ReactMapGL>
+        <main className="map">
+            <ReactMapGL
+                {...viewport}
+                mapStyle="mapbox://styles/mapbox/streets-v11"
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API}
+                onViewportChange={setViewport}
+            >
+                <Marker
+                    latitude={28.800791}
+                    longitude={-81.273109}
+                    offsetTop={-46}
+                    offsetLeft={-28}
+                >
+                    <img className="map-marker" src={mapMarker} alt="Map marker icon" />
+                </Marker>
+            </ReactMapGL>
+        </main>
+
     )
 }
 
