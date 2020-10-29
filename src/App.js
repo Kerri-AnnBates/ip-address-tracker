8455ad271b3be36c236f530cc04b965d069b0dc7
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-10-28 22:49:08 -0400

    initial set up of mapbox

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./components/Header";
 import MapDisplay from "./components/MapDisplay";
 import "./styles/main.scss";
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 
 const App = () => {
   return (
--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -1,30 +1,21 @@
 import React, { useState } from "react";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
-import { Icon } from "leaflet";
+import ReactMapGL from 'react-map-gl';
 
 const MapDisplay = () => {
-    const [lat, setLat] = useState(51.505);
-    const [lon, setLon] = useState(-0.09);
-
-    const position = [lat, lon];
-    const newMarker = new Icon({
-        iconUrl: require("../images/icon-location.svg"),
-        iconSize: [50]
+    const [viewport, setViewport] = useState({
+        height: "100vh",
+        width: "100vw",
+        latitude: 28.800791,
+        longitude: -81.273109,
+        zoom: 10
     });
 
     return (
-        // <div className="map" id="ip-map"></div>
-        <Map className="map" center={position} zoom={13}>
-            <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position} icon={newMarker}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </Map>
+        <ReactMapGL
+            className="map"
+            {...viewport}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API}
+        ></ReactMapGL>
     )
 }
 
